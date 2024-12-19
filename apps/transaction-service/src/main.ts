53c230d3781c4d783b6012fd735ea054f7638baf
slw <slw@cdcy.com>
2024-12-20 04:56:47 +0800

    feat: configure transaction service with RabbitMQ and MongoDB integration, update Docker setup

--- apps/transaction-service/src/main.ts
+++ apps/transaction-service/src/main.ts
@@ -1,17 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
+import { RmqService } from '@app/rmq';
 import { TransactionServiceModule } from './transaction-service.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(TransactionServiceModule, {
-    transport: Transport.TCP,
-    options: {
-      host: '0.0.0.0',
-      port: 3003,
-    },
-  });
-
+  const app = await NestFactory.create(TransactionServiceModule);
+  const rmqService = app.get<RmqService>(RmqService);
+  app.connectMicroservice(rmqService.getOptions('TRANSACTION'));
   app.useGlobalPipes(new ValidationPipe());
+  
+  await app.startAllMicroservices();
+  await app.listen(3003);
 }
 bootstrap();
